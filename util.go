63b70cb92b0f05e67bb87a472042155d425c4745
Ohad Rodeh <orodeh@dnanexus.com>
2019-12-05 11:01:17 -0800

    WIP

--- util.go
+++ util.go
@@ -23,7 +23,7 @@ const (
 	MaxDirSize         = 10 * 1000
 	MaxNumFileHandles  = 1000 * 1000
 	NumRetriesDefault  = 3
-	Version            = "v0.13"
+	Version            = "v0.14"
 )
 const (
 	InodeInvalid       = 0
