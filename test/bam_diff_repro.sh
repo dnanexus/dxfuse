b218fe55931fcc255eeb78cd23c6b1741236b473
Ohad Rodeh <orodeh@dnanexus.com>
2020-01-10 14:17:38 -0800

    WIP

--- test/bam_diff_repro.sh
+++ test/bam_diff_repro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo -E /go/bin/dxfuse -uid $(id -u) -gid $(id -g) ~/MNT dxfuse_test_data
+sudo -E /go/bin/dxfuse -verbose 1 -uid $(id -u) -gid $(id -g) ~/MNT dxfuse_test_data
 sample_name='SRR10270774'
 
 cd ~/MNT/dxfuse_test_data/reference_data/bam
@@ -11,4 +11,6 @@ bam2="${sample_name}_markdup.B.bam"
 ls -lh $bam $bam2
 rm -f ~/${sample_name}_diff.txt.gz
 
-time bam diff --in1 "$bam" --in2 "$bam2" --onlyDiffs --baseQual --tags MD:Z,NM:i,MQ:i,RG:Z,XA:Z,XS:i | gzip > ~/${sample_name}_diff.txt.gz
+#time bam diff --in1 "$bam" --in2 "$bam2" --onlyDiffs --baseQual --tags MD:Z,NM:i,MQ:i,RG:Z,XA:Z,XS:i | gzip > ~/${sample_name}_diff.txt.gz
+
+sambamba flagstat $bam -p > ~/sambamba_info.txt
