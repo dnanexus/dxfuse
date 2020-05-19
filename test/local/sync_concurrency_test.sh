8b6c53b4632cba7954cd1165252084d7b42fc696
Kurt Jensen <kjensen@kjdev.vdev.dnanexus.com>
2020-05-19 01:55:47 +0000

    Add local test for sync concurrency

---
+++ test/local/sync_concurrency_test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+######################################################################
+# global variables
+
+CRNT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mountpoint=${HOME}/MNT
+projName="dxfuse_test_data"
+dxfuse="$GOPATH/bin/dxfuse"
+teardown_complete=0
+
+writeable_dir=""
+######################################################################
+
+# cleanup sequence
+function teardown {
+    if [[ $teardown_complete == 1 ]]; then
+        return
+    fi
+    teardown_complete=1
+    rm -f 50MB
+    dx rm -r $projName:/$writeable_dir
+    echo "unmounting dxfuse"
+    cd $HOME
+    fusermount -u $mountpoint
+}
+
+# trap any errors and cleanup
+trap teardown EXIT
+
+# Ensure that only one sync command may run at a time
+function sync_concurrency_test {
+    mkdir -p $mountpoint
+    $dxfuse $mountpoint $projName
+    sleep 1
+    dd if=/dev/urandom of=50MB bs=1M count=50
+    writeable_dir=$(cat /dev/urandom | env LC_CTYPE=C LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+    dx mkdir $projName:/$writeable_dir
+    # Start copy and dxfuse sync
+    cp 50MB $mountpoint/$projName/$writeable_dir &
+    CP_PID=$!
+    $dxfuse -sync &
+    SYNC_PID=$!
+    sleep 1
+    # Additional sync commands fail until the above sync completes
+    sync_output=$($dxfuse -sync 2>&1)
+
+    if echo "$sync_output" | grep -q "another sync operation is already running"; then
+        wait $CP_PID
+        wait $SYNC_PID
+        echo "$sync_output"
+        # Sync should function again now that the first sync command has completed
+        $dxfuse -sync
+    else
+        echo "Second sync operation did not error out"
+        exit 1
+    fi
+
+    teardown
+}
