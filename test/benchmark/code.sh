3f62735984127b80046ecb4bb1c8e6ff0c62510c
Kurt Jensen <kjensen@dnanexus.com>
2021-01-07 02:32:44 +0000

    WIP

--- test/benchmark/code.sh
+++ test/benchmark/code.sh
@@ -123,11 +123,11 @@ main() {
     # download with dxfuse
     # Start the dxfuse daemon in the background, and wait for it to initilize.
     echo "Mounting dxfuse"
-    flags=""
+    flags="-readWrite"
     if [[ $verbose != "" ]]; then
-        flags="-verbose 1"
+        flags="$flags -verbose 1"
     fi
-    dxfuse -readWrite $flags $mountpoint $DX_PROJECT_CONTEXT_ID
+    dxfuse $flags $mountpoint $DX_PROJECT_CONTEXT_ID
     sleep 1
     projName=$(ls $mountpoint)
     echo "projName = $projName"
