09983c2be62d805ff6101a3f8d55dba2b117e6e3
Kurt Jensen <kjensen@dnanexus.com>
2025-03-07 16:33:01 -0500

    WIP

--- test/local/multiple_mounts.sh
+++ test/local/multiple_mounts.sh
@@ -69,11 +69,5 @@ function multiple_mounts {
         exit 1
     fi
 
-    # check that the mounts are the same
-    if ! diff <(ls -l "$mountpoint0/$projName") <(ls -l "$mountpoint1/$projName"); then
-        echo "Error: The two mounts are different."
-        exit 1
-    fi
-
     teardown
 }
