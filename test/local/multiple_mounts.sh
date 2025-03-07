450d30894ee4652c6cd46e7bae3cfa6d9ab8d3d2
Kurt Jensen <kjensen@dnanexus.com>
2025-03-07 15:53:11 -0500

    WIP

--- test/local/multiple_mounts.sh
+++ test/local/multiple_mounts.sh
@@ -2,6 +2,7 @@ CRNT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 projName="dxfuse_test_data"
 dxfuse="$CRNT_DIR/../../dxfuse"
 baseDir=$HOME/dxfuse_test
+state_folder="$baseDir/.dxfusebase"
 mountpoint0=${baseDir}/MNT
 mountpoint1=${baseDir}/MNT1
 
@@ -17,6 +18,7 @@ function teardown {
     cd $HOME
     fusermount -u $mountpoint
     fusermount -u $mountpoint1
+    rm -rf $state_folder
 }
 
 # trap any errors and cleanup
@@ -29,7 +31,6 @@ function multiple_mounts {
     # mount dxfuse with default folder path
     $dxfuse $mountpoint0 $projName
 
-    state_folder="$baseDir/.dxfusebase"
     # mount dxfuse with a different folder path
     $dxfuse -stateFolder $state_folder $mountpoint1 $projName
 
@@ -45,7 +46,9 @@ function multiple_mounts {
     # check that two dxfuse mounts are present
     mount_count=$(mount | grep -c dxfuse)
     if [[ $mount_count -eq 2 ]]; then
-        echo "Two dxfuse mounts are present."
+        echo "Two dxfuse mounts are present"
+        echo "Mounts:"
+        mount | grep dxfuse
     else
         echo "Error: Expected 2 dxfuse mounts, but found $mount_count."
         exit 1
@@ -53,16 +56,16 @@ function multiple_mounts {
 
     # check that dxfuse state files exist in both folders
     if [[ -f $state_folder/dxfuse.log ]]; then
-        echo "Log file exists in $state_folder"
+        echo "Log file exists with -stateFolder arg $state_folder"
     else
         echo "Log file does not exist in $state_folder"
         exit 1
     fi
 
-    if [[ -f "$HOME/dxfuse.log" ]]; then
-        echo "Log file exists in $HOME"
+    if [[ -f /root/.dxfuse/dxfuse.log ]]; then
+        echo "Log file exists with default -stateFolder path"
     else
-        echo "Log file not exist in $HOME"
+        echo "Log file not exist in /root/.dxfuse"
         exit 1
     fi
 
