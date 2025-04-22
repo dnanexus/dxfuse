9929cb08fb9d0129d70e39facdd53878ef6f0d7a
Kurt Jensen <kjensen@dnanexus.com>
2025-04-22 13:05:39 +0000

    Create 25 files

--- test_parallel_rw.sh
+++ test_parallel_rw.sh
@@ -9,7 +9,7 @@ rm -rf /home/kjensen/MNT/testing/1kib*
 # read 5 1GiB files
 for i in {1..5}; do cat /home/kjensen/MNT/testing/1gb$i >/dev/null & done
 # # write 15 1GiB files
-for i in {1..5}; do dd if=/dev/zero of=/home/kjensen/MNT/testing/1GiB$i bs=1M count=1024 & done
+for i in {1..25}; do dd if=/dev/zero of=/home/kjensen/MNT/testing/1GiB$i bs=1M count=1024 & done
 
 # time and create 300 1kib files in parallel
 #for i in {1..300}; do dd if=/dev/zero of=/home/kjensen/MNT/testing/1kib$i bs=1K count=1 & done
